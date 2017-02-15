520001e47a11bf7d8df287116ab820ebdc488f16
Paul Walsh <twentytwentyk@live.com>
2017-02-15 16:09:42 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
+$(function() {
+
+  $("div.switch > button").click(function() {
+    var button = $("div.switch > button")
+    if (button.hasClass('on')){
+    button.removeClass("on").addClass("off");
+    $('body').removeClass("light").addClass("dark");
+    $('.status').empty().append("Hey, who turned the lights off mate?");
+  }
+    else {
+    button.removeClass("off").addClass("on");
+    $('body').removeClass("dark").addClass("light");
+    $('.status').empty().append("It's so bright in here!");
+
+  }
+    // if on turn off
+    // else if off turn on
+  });
 
 
+
+});
